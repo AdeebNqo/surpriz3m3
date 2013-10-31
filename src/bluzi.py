7a21d9af8e26a63e0c9b047e3ba3d8d9b1d2e9dd
AdeebNqo <adeebnqo@gmail.com>
2013-10-31 16:11:01 +0200

    testing sending data to my phone

--- src/bluzi.py
+++ src/bluzi.py
@@ -1,26 +1,25 @@
 import bluetooth
-print("searching for devices...")
-devices = bluetooth.discover_devices()
-print("found 'em")
-for device in devices:
-	print(bluetooth.lookup_name(device))
 class transfer:
 	#
 	# Creating data transfer object
-		self.address = address
 	# port is always 1 and address has to be provided
 	def __init__(self, address):
 		self.port = 1
-		self.address = ""
+		self.address = address
+		self.socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+		self.socket.connect((address, self.port))
 	#
 	# Method for sending data to
 	# default address
 	#
 	def send(self, filepath):
+		f = open(filepath)
+		send(f)
 		print("sending...")
 	#
 	# Method for sending data. This
 	# changes address
 	#
 	def send(self, filepath, address):
-		self.address = 
+		self.address = address
+
--- src/main.py
+++ src/main.py
@@ -2,5 +2,13 @@
 # Testing
 #
 import notif
+import bluetooth
+from bluzi import transfer
 n = notif.notif("/dev/nokia", "Nokia phone")
 n.notify()
+try:
+	d = bluetooth.discover_devices()
+	trans = transfer(d[0])
+	trans.send("/home/adeeb/Documents/programming/surpriz3m3/src")
+except bluetooth.btcommon.BluetoothError:
+	print("Could not connect")
