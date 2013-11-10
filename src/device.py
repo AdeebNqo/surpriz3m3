c54a6c31efd07bca80fb07b924c481421843dca3
AdeebNqo <adeebnqo@gmail.com>
2013-11-10 15:57:16 +0200

    bluetooth failing

--- src/device.py
+++ src/device.py
@@ -1,4 +1,4 @@
-import bluetooth
+import lightblue
 class device:
 	#
 	# Creating data transfer object address has to be provided
@@ -12,48 +12,5 @@ class device:
 	# Method for getting device services and their ports
 	#
 	def get_services(self):
-		services = bluetooth.find_service(address=self.address)
+		services = lightblue.findservices(self.address)
 		return services
-	#
-	# Method for retrieving all supported ports
-	# -keeps p
-	def get_supported_ports(self):
-		services = self.get_services()
-		for service in services:
-			self.ports[service['name']] = service['port']
-		return self.ports
-	#
-	# Method for checking if file transfer is supported by device
-	#
-	def is_filetransfer_supported(self):
-		services = self.get_services();
-		for service in services:
-			if (service['name']=="OBEX File Transfer"):
-				return True
-		return False
-	#
-	# Method for getting port for file transfer port
-	#
-	def get_filetransfer_port(self):
-		return ports['OBEX File Transfer']
-	#
-	# Method for connecting to device for particular
-	# service hence the port of the service is needed
-	#
-	def connect(self, port):
-		self.socket.connect((self.address, port))
-	#
-	# Method for sending data to
-	# default address
-	#
-	def send(self, filepath):
-		f = open(filepath)
-		send(f)
-		print("sending...")
-	#
-	# Method for sending data. This
-	# changes address
-	#
-	def send(self, filepath, address):
-		self.address = address
-
--- src/main.py
+++ src/main.py
@@ -2,28 +2,24 @@
 # Testing
 #
 from notifications import notification
-import bluetooth
 from device import device
 from actions import actions
+import lightblue
 
 def yes():
 	print('yes!')
-try:
-	#
-	# Testing notifications
-	#
-	responses = actions(True)
-	responses.funclist['yes'] = yes
-	responses.funclist['no'] = yes
-	notif = notification("Zola")
-	notif.notify("This is a notification", notification.info, responses)
-	#
-	# Testing device connections
-	#
-	d = bluetooth.discover_devices()
-	trans = device(d[0])
-	print(trans.get_services())
-	trans.connect(10)
-	#trans.send("/home/adeeb/Documents/programming/surpriz3m3/src/setup.py")
-except bluetooth.btcommon.BluetoothError as this:
-	print("Could not connect %s " %this)
+
+#
+# Testing notifications
+#
+
+#responses = actions(True)
+#responses.funclist['yes'] = yes
+#responses.funclist['no'] = yes
+#notif = notification("Zola")
+#notif.notify("This is a notification", notification.info, responses)
+
+#
+# Testing device connections
+#
+print(lightblue.finddevices())
