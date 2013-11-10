4f133f8d134df42b0c601b541c91e8a0c9ea15da
AdeebNqo <adeebnqo@gmail.com>
2013-11-10 16:24:33 +0200

    changed device class to work with lightblue

--- src/device.py
+++ src/device.py
@@ -4,13 +4,25 @@ class device:
 	# Creating data transfer object address has to be provided
 	#
 	def __init__(self, address):
-		self.ports = {}
-		self.port = 1
+		self.service_ports = {}
 		self.address = address
-		self.socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+		self.ports_discovered = false
 	#
 	# Method for getting device services and their ports
 	#
 	def get_services(self):
 		services = lightblue.findservices(self.address)
 		return services
+	#
+	# Method for determining/finding service ports
+	#
+	def discover_service_ports(self):
+		for service in self.get_services():
+			self.service_ports[service[2]] = service[1]
+	#
+	# Method for retrieving service ports
+	# @returns dictionary with port numbers, service name is the key
+	def get_service_ports(self):
+		if (!ports_discovered):
+			self.discover_service_ports()
+		return service_ports
--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 #
 from notifications import notification
 from device import device
-from actions import action
+from actions import actions
 import lightblue
 import time
 
@@ -24,13 +24,18 @@ def yes():
 # Testing device connections
 #
 
+selected_device = None
 found_devices=[]
 for i in range(2):
 	print("searching for bt device...")
 	found_devices = lightblue.finddevices()
 	if (len(found_devices) > 0):
+		selected_device = lightblue.selectdevice()
 		break
 	print('No devices found')
 	if (i!=2):
 		print("will retry in 20 seconds.")
 		time.sleep(20)
+
+if (selected_device==None):
+	print('Issues with blutooth? Eish...suckz bein\' u.')
